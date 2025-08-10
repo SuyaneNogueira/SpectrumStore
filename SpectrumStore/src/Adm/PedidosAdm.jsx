e6596aa38bbe3bc1724e24e4fbbcf46d22f426ef
Manuel-Gon <manumgoya73@gmail.com>
2025-08-10 19:33:52 -0300

    Amoeba

---
+++ SpectrumStore/src/Adm/PedidosAdm.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './PedidosAdm.css'
+
+function PedidosAdm() {
+  return (
+    <div className='background-adm-pordutos'>
+      
+       <div className='layout-modal-adm'>
+             <div className='logo-store-adm'>
+
+             </div>
+       </div>
+
+    </div>
+  )
+}
+
+export default PedidosAdm
--- SpectrumStore/src/Router/Router.jsx
+++ SpectrumStore/src/Router/Router.jsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Tela_inicial from "../Tela inicial/Tela_inicial"
+import PedidosAdm from "../Adm/PedidosAdm";
 
 const Router = createBrowserRouter([
-{path: "/", element: <Tela_inicial/>}
+{path: "/", element: <Tela_inicial/>},
+{path: "/Adm", element: <PedidosAdm/>}
+
 
 
 ])
