42a9d1cf15c64a9ee48c6070819e58ef89cf2237
SuyaneNogueira <naolhenaofale7@gmail.com>
2025-08-11 17:17:28 -0300

    Criação do Carrinho de Compras

---
+++ SpectrumStore/src/Cliente/Carrinho/CarrnhoP1.jsx
@@ -0,0 +1,30 @@
+import './CarrinhoP1.css'
+import Navbar from '../../Navbar/Navbar'
+function CarrnhoP1() {
+  return (
+    <div className='fundoCarrinho'>
+     <Navbar/>
+     <div className='ibovespa'>
+      <img src="CarrinhoFundooooo.png" alt="" className='foto-de-fundo-carrinho' />
+        <div className='sssssss'>
+           <div className='CoisaBrancaCarrinho'>coisa branca
+            <div>Comprar tudo</div>
+            <div>card1</div>  
+            <div>card2</div>  
+            <div>card3</div> 
+            <div>card4</div>   
+            <div>card5</div>
+            <div>botao
+                <div>botao1</div>
+                <div>botao2</div>
+            </div>
+        </div>  
+        </div>
+
+      </div> 
+
+    </div>
+  )
+}
+
+export default CarrnhoP1
--- SpectrumStore/src/Router/Router.jsx
+++ SpectrumStore/src/Router/Router.jsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Tela_inicial from "../Tela inicial/Tela_inicial"
 import PedidosAdm from "../Adm/PedidosAdm";
+import CarrnhoP1 from "../Cliente/Carrinho/CarrnhoP1";
 
 const Router = createBrowserRouter([
 {path: "/", element: <Tela_inicial/>},
-{path: "/Adm", element: <PedidosAdm/>}
-
+{path: "/Adm", element: <PedidosAdm/>},
+{path: "/Carrinho", element: <CarrnhoP1/>}
 
 
 ])
