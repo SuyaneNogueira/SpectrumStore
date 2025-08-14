b12577ec9dbecbc1b919ceb3472ab4aaa8b9b376
MANUEL ALEJANDRO MUÑOZ GONZÁLEZ <manuel_gonzalez@estudante.sesisenai.org.br>
2025-08-14 16:39:11 -0300

    Mais coisinhas ADM

--- SpectrumStore/src/Adm/pages/Pedidos.jsx
+++ SpectrumStore/src/Adm/pages/Pedidos.jsx
@@ -2,8 +2,14 @@ import "./Pedidos.css";
 import { CgProfile } from "react-icons/cg";
 import { CiSearch } from "react-icons/ci";
 import { FaRegBell } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 export default function Pedidos() {
+
+  const navigate = useNavigate();
+
   return (
     <div className="pedidos-visao-adm">
       <div className="titulo-pedidos-adm">
@@ -16,7 +22,7 @@ export default function Pedidos() {
         <div className="icones-geral-adm">
           <div className="icons-notification-adm">
             <FaRegBell
-              size={40}
+              size={30}
               color="#03374C"
               style={{ cursor: "pointer" }}
               onClick={() => navigate("/")}
@@ -25,7 +31,7 @@ export default function Pedidos() {
 
           <div className="icon-search-adm">
             <CiSearch
-               size={40}
+               size={30}
               color="#03374C"
               style={{ cursor: "pointer" }}
               onClick={() => navigate("/")}
@@ -34,7 +40,7 @@ export default function Pedidos() {
 
           <div className="icon-perfil-adm">
             <CgProfile
-              size={40}
+              size={30}
               color="#03374C"
               style={{ cursor: "pointer" }}
               onClick={() => navigate("/")}
@@ -42,6 +48,25 @@ export default function Pedidos() {
           </div>
         </div>
       </div>
+
+     <div className="area-dos-pedidos-adm">
+         <div className="titulo-dois-pedidos-adm">
+
+           <div> <Link to ="">Todos os Pedidos</Link> </div>
+           <div> <Link to ="">Processo</Link> </div>
+           <div> <Link to ="">Despacho</Link> </div>
+           <div> <Link to ="">Completo</Link> </div>
+
+         </div>
+         <div className="area-processos-pedidos-adm">
+               <div className="status-pedidos-adm-um">
+                   <p>ID</p>
+                   <p>Nome</p>
+                   <p>Endereço</p>
+               </div>
+         </div>
+     </div>
+
     </div>
   );
 }
