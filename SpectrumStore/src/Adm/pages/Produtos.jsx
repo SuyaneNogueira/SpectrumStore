6730aa9dca77b5bfb36423c33514dc5e2c23db32
Manuel-Gon <manumgoya73@gmail.com>
2025-08-17 18:37:41 -0300

    Continuando a fazer a parte de adm

--- SpectrumStore/src/Adm/pages/Produtos.jsx
+++ SpectrumStore/src/Adm/pages/Produtos.jsx
@@ -1,9 +1,51 @@
+import "./Produtos.css";
+import { CgProfile } from "react-icons/cg";
+import { CiSearch } from "react-icons/ci";
+import { FaRegBell } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
 export default function Produtos() {
+  const navigate = useNavigate();
   return (
-    <>
-      <h1>Produtos</h1>
-      
-     bhfdbshfbhb
-    </>
-  )
+    <div className="container-produtos-adm">
+      <div className="titulo-produtos-adm">
+        <div>
+          <h1>Produtos</h1>
+          <p>2 produtos cadastrados</p>
+        </div>
+        <div className="icones-geral-adm">
+          <div className="icons-notification-adm">
+            <FaRegBell
+              size={30}
+              color="#03374C"
+              style={{ cursor: "pointer" }}
+              onClick={() => navigate("/")}
+            />
+          </div>
+
+          <div className="icon-search-adm">
+            <CiSearch
+              size={30}
+              color="#03374C"
+              style={{ cursor: "pointer" }}
+              onClick={() => navigate("/")}
+            />
+          </div>
+
+          <div className="icon-perfil-adm">
+            <CgProfile
+              size={30}
+              color="#03374C"
+              style={{ cursor: "pointer" }}
+              onClick={() => navigate("/")}
+            />
+          </div>
+        </div>
+        
+      </div>
+      <div className="cadastro-dos-produtos-adm">
+
+      </div>
+    </div>
+  );
 }
