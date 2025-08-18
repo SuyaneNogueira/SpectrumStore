c57c2e617b4b0efe8e613f2cbc29bdbd15eb67dd
SuyaneNogueira <naolhenaofale7@gmail.com>
2025-08-18 17:18:46 -0300

    Carrinho

--- SpectrumStore/src/Cliente/Carrinho/CarrnhoP1.jsx
+++ SpectrumStore/src/Cliente/Carrinho/CarrnhoP1.jsx
@@ -11,22 +11,22 @@ function CarrnhoP1() {
         <div className='sssssss'>
            <div className='CoisaBrancaCarrinho'>
             <div className='Comprar-tudo'>
-              <div className='div-checkbox-comprar-tudo'><input  type="checkbox"/></div></div>
+              <div className='div-checkbox-comprar-tudo'><input className='Comprar-tudo-checkbox' type="checkbox"/></div></div>
 
             <div className='Card1'>
-              <div className='div-checkbox-card1'><input className='checkbox-ajusts' type="checkbox"/></div>
+              <div className='div-checkbox-card1'><input className='checkbox-card' type="checkbox"/></div>
               </div>  
 
             <div className='Card2'>
-              <div className='div-checkbox-card2'><input type="checkbox"/></div>
+              <div className='div-checkbox-card2'><input className='checkbox-card' type="checkbox"/></div>
               </div>  
 
             <div className='Card3'>
-              <div className='div-checkbox-card3'><input type="checkbox"/></div>
+              <div className='div-checkbox-card3'><input className='checkbox-card' type="checkbox"/></div>
               </div> 
 
             <div className='Card4'>
-              <div className='div-checkbox-card4'><input type="checkbox"/></div>
+              <div className='div-checkbox-card4'><input className='checkbox-card' type="checkbox"/></div>
               </div>   
             {/* <div>
                 <div>botao1</div>
