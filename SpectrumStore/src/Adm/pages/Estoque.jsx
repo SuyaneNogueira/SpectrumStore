aad28fa4690b960ada66a8af6bb8df0bcb54ecc9
Manuel-Gon <manumgoya73@gmail.com>
2025-09-06 22:29:46 -0300

    Fazendo o estoque pra adiantar

--- SpectrumStore/src/Adm/pages/Estoque.jsx
+++ SpectrumStore/src/Adm/pages/Estoque.jsx
@@ -1,9 +1,56 @@
+import "./Estoque.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
+import { CiSearch } from "react-icons/ci";
+import { FaRegBell } from "react-icons/fa";
+
 export default function Estoque() {
+
+const navigate = useNavigate();
+
+
   return (
-    <>
-      <h1>Estoque</h1>
-      
-     bhfdbshfbhb
-    </>
-  )
+    <div className="container-geral-estoque-adm">
+      <div className="titulo-estoque-adm">
+        <div className="titulo-definitivo-estoque">
+           <h1>Produtos</h1>
+        </div>
+
+        <div className="icones-estoque-adm">
+          <div className="icons-notification-adm">
+            <FaRegBell
+              size={30}
+              color="#03374C"
+              style={{ cursor: "pointer" }}
+              onClick={() => navigate("/")}
+            />
+          </div>
+
+          <div className="icon-search-adm">
+            <CiSearch
+              size={30}
+              color="#03374C"
+              style={{ cursor: "pointer" }}
+              onClick={() => navigate("/")}
+            />
+          </div>
+
+          <div className="icon-perfil-adm">
+            <CgProfile
+              size={30}
+              color="#03374C"
+              style={{ cursor: "pointer" }}
+              onClick={() => navigate("/")}
+            />
+          </div>
+        </div>
+
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
