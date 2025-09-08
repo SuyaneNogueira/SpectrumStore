6a69349c141a0e3761d1e1b6d828b3f8a3cb6935
Manuel-Gon <manumgoya73@gmail.com>
2025-09-07 21:19:58 -0300

    Fazendo a aba de estoque para adiantar

--- SpectrumStore/src/Adm/pages/Estoque.jsx
+++ SpectrumStore/src/Adm/pages/Estoque.jsx
@@ -1,54 +1,60 @@
 import "./Estoque.css";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import { Link } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { CiSearch } from "react-icons/ci";
 import { FaRegBell } from "react-icons/fa";
 
 export default function Estoque() {
-
-const navigate = useNavigate();
-
+  const navigate = useNavigate();
 
   return (
     <div className="container-geral-estoque-adm">
       <div className="titulo-estoque-adm">
         <div className="titulo-definitivo-estoque">
-           <h1>Produtos</h1>
+          <h1>Estoque</h1>
+          {/* {} */}
+          <div className="icones-estoque-adm">
+            <div className="icons-notification-adm-estoque">
+              <FaRegBell
+                size={30}
+                color="#03374C"
+                style={{ cursor: "pointer" }}
+                onClick={() => navigate("/")}
+              />
+            </div>
+
+            <div className="icon-search-adm-estoque">
+              <CiSearch
+                size={30}
+                color="#03374C"
+                style={{ cursor: "pointer" }}
+                onClick={() => navigate("/")}
+              />
+            </div>
+
+            <div className="icon-perfil-adm-estoque">
+              <CgProfile
+                size={30}
+                color="#03374C"
+                style={{ cursor: "pointer" }}
+                onClick={() => navigate("/")}
+              />
+            </div>
+          </div>
         </div>
 
-        <div className="icones-estoque-adm">
-          <div className="icons-notification-adm">
-            <FaRegBell
-              size={30}
-              color="#03374C"
-              style={{ cursor: "pointer" }}
-              onClick={() => navigate("/")}
-            />
+        <div className="div-info-estoque-res">
+          <div className="div-info-produtos-estoque">
+            <div className="div-enfeite-produtos-em-estoque">
+              <p>Produtos em estoque:8</p>
+            </div>
           </div>
 
-          <div className="icon-search-adm">
-            <CiSearch
-              size={30}
-              color="#03374C"
-              style={{ cursor: "pointer" }}
-              onClick={() => navigate("/")}
-            />
+          <div className="div-info-produtos-fora-estoque">
+            <div className="div-enfeite-produtos-fora-estoque">
+              <p>Fora estoque:3</p>
+            </div>
           </div>
-
-          <div className="icon-perfil-adm">
-            <CgProfile
-              size={30}
-              color="#03374C"
-              style={{ cursor: "pointer" }}
-              onClick={() => navigate("/")}
-            />
-          </div>
-        </div>
-
-        <div>
-          
         </div>
       </div>
     </div>
