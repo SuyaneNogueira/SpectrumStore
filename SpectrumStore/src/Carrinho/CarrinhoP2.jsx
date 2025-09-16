7579c6ff08f615e515e98a3fb41eb89213e83ad1
MARIA EDUARDA KNUPP RATTO <maria_ratto@estudante.sesisenai.org.br>
2025-09-16 17:15:17 -0300

    Carrinho

--- SpectrumStore/src/Carrinho/CarrinhoP2.jsx
+++ SpectrumStore/src/Carrinho/CarrinhoP2.jsx
@@ -1,68 +1,143 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './CarrinhoP2.css';
 import Navbar from '../Navbar/Navbar';
-import StarRating from '../TelaInicial/StarRating';
-import { CartContext } from './CartContext'; 
-import TrashIcon from '../imagens/trash-icon.png'; 
+import { useCart } from './CartContext';
 import { Link } from 'react-router-dom';
+import CartaoCredito from '../imagens/cartao-credito.png';
+import CartaoDebito from '../imagens/cartao-debito.png';
+import Pix from '../imagens/pix.png';
+import Visa from '../imagens/visa.png';
+import Nubank from '../imagens/nubank.png';
+import Mastercard from '../imagens/mastercard.png';
+import Bradesco from '../imagens/bradesco.png';
 
 function CarrinhoP2() {
-    const { cartItems, removeFromCart, toggleAllItems, toggleItem, selectAll, totalSelected } = useContext(CartContext);
-  return (
- <div className='fundoPagamento'>
-  <Navbar/>
-  <div className='div-conteinerDeFora'>
-    <div className='FundoBanco'> 
-      <div className='Voltar'><Link to='/Carrinho'>dd</Link></div>
-      <div className='div-conteinerDosProtudos'>
-     <div className='div-protudos'>
-      {cartItems && cartItems.map((item, index)=> 
-      <div className='Protudos' key={item.cartItemId || index}>
-      <div className='imagem-protudo-pagamento'>
-      <img className='imagem-pagamento' src={item.image} alt={item.name} />
-      </div>
-      <div className='infomações-protudo-pagamento'>
-         <div className='Nome-Protudo-pagamento'>
-          <p className='nome-pagamento'>{item.name}</p>
+    const { cartItems } = useCart();
+    
+    // Simulação dos valores de desconto e frete, você pode ajustar
+    const frete = 20.00;
+    const desconto = 50.00;
+    
+    // Cálculo do total dos produtos (considera apenas itens com quantidade)
+    const totalProdutos = cartItems.reduce((total, item) => total + (item.price * (item.quantidade || 1)), 0);
+
+    // Cálculo do total final
+    const totalFinal = totalProdutos + frete - desconto;
+
+    return (
+        <div className='fundoPagamento'>
+            <Navbar />
+            <div className='div-conteinerDeFora'>
+                <div className='FundoBanco'>
+                    <div className='Voltar'>
+                        <Link to='/Carrinho'>Voltar</Link>
+                    </div>
+                    
+                    {/* Container de Produtos */}
+                    <div className='div-conteinerDosProtudos'>
+                        <div className='div-protudos'>
+                            {cartItems && cartItems.map((item, index) =>
+                                <div className='Protudos' key={item.cartItemId || index}>
+                                    <div className='imagem-protudo-pagamento'>
+                                        <img className='imagem-pagamento' src={item.image} alt={item.name} />
+                                    </div>
+                                    <div className='infomações-protudo-pagamento'>
+                                        <div className='Nome-Protudo-pagamento'>
+                                            <p className='nome-pagamento'>{item.name}</p>
+                                        </div>
+                                        <div className='valor-briquedo-pagamento'>
+                                            <span className='cor-amarelo-preco-2-pagamento'>R$:</span> <span className='preço-pagamento-protudos'>{item.price.toFixed(2)}</span>
+                                        </div>
+                                        <div className='quantidade-pagamento'>
+                                            {item.quantidade} unidade(s)
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+
+                    {/* Container de Pagamento e Resumo */}
+                    <div className="resumo-e-botoes-compra">
+                        
+                        {/* Seção de Formas de Pagamento */}
+                        <div className="forma-pagamento">
+                            <h2>Formas de Pagamentos</h2>
+                            <div className="opcoes-pagamento">
+                                {/* Opção de Cartão de Crédito */}
+                                <label className="opcao-item">
+                                    <input type="radio" name="formaPagamento" value="credito" />
+                                    <div className="info-pagamento">
+                                        <div className="icone-pagamento">
+                                            <img src={CartaoCredito} alt="Cartão de Crédito" className='img-cartao-credito' />
+                                        </div>
+                                        <div className="texto-pagamento">
+                                            <h4>Cartões de crédito aceitos</h4>
+                                            <div className="bandeiras">
+                                                <img src={Visa} alt="Visa" />
+                                                <img src={Nubank} alt="Nubank" />
+                                                <img src={Mastercard} alt="Mastercard" />
+                                                <img src={Bradesco} alt="Bradesco" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </label>
+
+                                {/* Opção de Cartão de Débito */}
+                                <label className="opcao-item">
+                                    <input type="radio" name="formaPagamento" value="debito" />
+                                    <div className="info-pagamento">
+                                        <div className="icone-pagamento">
+                                            <img src={CartaoDebito} alt="Cartão de Débito" className='img-cartao-debito' />
+                                        </div>
+                                        <div className="texto-pagamento">
+                                            <h4>Cartões de débito aceitos</h4>
+                                            <div className="bandeiras">
+                                                <img src={Visa} alt="Visa" />
+                                                <img src={Nubank} alt="Nubank" />
+                                                <img src={Mastercard} alt="Mastercard" />
+                                                <img src={Bradesco} alt="Bradesco" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </label>
+
+                                {/* Opção de Pix */}
+                                <label className="opcao-item">
+                                    <input type="radio" name="formaPagamento" value="pix" />
+                                    <div className="info-pagamento">
+                                        <div className="icone-pagamento">
+                                            <img src={Pix} alt="Pix" className='img-pix' />
+                                        </div>
+                                        <div className="texto-pagamento">
+                                            <h4>Pix</h4>
+                                        </div>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className="resumo-e-botoes-acoes">
+                            <div className="total-geral">
+                                <div className="resumo-valores">
+                                    <p>Total dos Produtos: R${totalProdutos.toFixed(2)}</p>
+                                    <p>Total de desconto: R${desconto.toFixed(2)}</p>
+                                    <p>Frete: R${frete.toFixed(2)}</p>
+                                </div>
+                                <div className="total-final">
+                                    <p>Total Geral: R${totalFinal.toFixed(2)}</p>
+                                </div>
+                            </div>
+                            <div className="botao-pagar-cancelar-compra">
+                                <button className='botao-cancelar'>Cancelar Compra</button>
+                                <button className='botao-pagar'>Pagar Compra</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className='valor-briquedo-pagamento'>
-          <span className='cor-amarelo-preco-2-pagamento'>R$:</span> <span className='preço-pagamento-protudos'>{item.price.toFixed(2)}</span>
-        </div>
-        <div className='quantidade-pagamento'>
-          ggggg
-        </div>
-      </div>
-        
-      </div>
-        )}
-     </div>
-      </div>
-    <div className="Card2-pagamento">
-      <div className="forma-pagamento-desistir">
-        <div className="forma-pagamento"></div>
-        <div className="botao-cancelar-compra">
-        <button className='botao-cancelar'>Cancelar Compra</button>
-        </div>
-
-      </div>
-
-      <div className="total-geral-desconto-pagar">
-        <div className="total-geral"></div>
-        <div className="botao-pagar-compra">
-         <button className='botao-pagar'>Pagar Compra</button> 
-        </div>
-        
-      </div>
-
-    </div>
-
-    </div>
-
-  </div>
-
-
- </div>
-  )
+    );
 }
 
-export default CarrinhoP2
+export default CarrinhoP2;
--- SpectrumStore/src/Carrinho/CarrnhoP1.jsx
+++ SpectrumStore/src/Carrinho/CarrnhoP1.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './CarrinhoP1.css';
 import Navbar from '../Navbar/Navbar';
 import StarRating from '../TelaInicial/StarRating';
@@ -10,104 +10,116 @@ function CarrnhoP1() {
   const { cartItems, removeFromCart, toggleAllItems, toggleItem, selectAll, totalSelected, updateQuantity } = useCart();
 
   return (
-  <div className='fundoCarrinho'>
-     <Navbar/>
-     <div className='sssssss'>
-       <div className='CoisaBrancaCarrinho'>
-         <div className='Comprar-tudo'>
-         <div className="content">
-           <label className="checkBox">
-             <input 
-               id="ch-tudo" 
-               type="checkbox" 
-               checked={selectAll}
-               onChange={(e) => toggleAllItems(e.target.checked)}
-             />
-             <div className="transition" />
-           </label>
-         </div>
-         <span className='comprar-tudo-div'>Comprar tudo</span>
-         </div>
-         <div className="lista-produtos-scroll">
-         {cartItems && cartItems.map((item, index) => (
-         <div className='Card1' key={item.cartItemId || `item-${index}`}>
-            <div className='div-checkbox-e-imagem'>
-              <div className="content">
-                <label className="checkBox">
+    <div className='fundoCarrinho'>
+      <Navbar/>
+      <div className='sssssss'>
+        <div className='CoisaBrancaCarrinho'>
+          <div className='Comprar-tudo'>
+            <div className="content">
+              <label className="checkBox">
                 <input 
-                   id={`ch-${item.cartItemId}`} 
-                   type="checkbox" 
-                   checked={item.isSelected}
-                   onChange={() => toggleItem(item.cartItemId)}
+                  id="ch-tudo" 
+                  type="checkbox" 
+                  checked={selectAll}
+                  onChange={(e) => toggleAllItems(e.target.checked)}
                 />
                 <div className="transition" />
-                </label>
-              </div>
-              <div className='div-imagem-produto-carrinho'>
-                <div className='imagem-carrinho-logo'>
-                <img className='imagem-mesmo-produtos-carrinho' src={item.image} alt={item.name} />
-                </div>
-              </div> 
-              <div className='Descrição-estrela'> 
-                <div className='estrela'>
-                <StarRating rating={item.rating} />
-                </div>
-                <div className='descrição'>
-                <p>{item.name}</p>
-                {item.personalizacoes && Object.entries(item.personalizacoes).length > 0 && (
-                   <div className="personalizacoes-carrinho">
-                     {Object.entries(item.personalizacoes).map(([key, value]) => (
-                       <div key={key} className="personalizacao-item-carrinho">
-                       <span className="personalizacao-chave">{key.charAt(0).toUpperCase() + key.slice(1)}:</span>
-                       {key === 'cor' || key === 'corFundo' || key === 'corPrincipal' ? (
-                          <div 
-                            className="cor-carrinho" 
-                            style={{ backgroundColor: value.toLowerCase().replace(/á/g, 'a').replace(/é/g, 'e') }}
-                            title={value}
-                          />
-                       ) : (
-                          <span className="personalizacao-valor">{value}</span>
-                       )}
-                       </div>
-                     ))}
-                   </div>
-                )}
-                </div>
-                <div className='div-valor-brinquedo-carrinho'>
-                   <span className='cor-amarelo-preco-2'>R$:</span> {(item.price * (item.quantidade || 1)).toFixed(2)}
-                </div>
-                {/* NOVO: Seletor de quantidade no carrinho */}
-                <div className="seletor-quantidade">
-                    <button onClick={() => updateQuantity(item.cartItemId, Math.max(1, (item.quantidade || 1) - 1))}>-</button>
-                    <input 
-                        type="number" 
-                        value={item.quantidade || 1} 
-                        readOnly 
-                        min="1"
-                    />
-                    <button onClick={() => updateQuantity(item.cartItemId, (item.quantidade || 1) + 1)}>+</button>
-                </div>
-              </div>
-              <div className='div-trash-icon'>
-                <button onClick={() => removeFromCart(item.cartItemId)}>
-                <img src={TrashIcon} alt="Remover" className="trash-icon" />
-                </button>
-              </div>
+              </label>
             </div>
-         </div>
-         ))}
-       </div>
-         <div className='div-button-carrinho'>
+            <span className='comprar-tudo-div'>Comprar tudo</span>
+          </div>
+          
+          <div className="lista-produtos-scroll">
+            {cartItems && cartItems.map((item, index) => (
+              <div className='Card1' key={item.cartItemId || `item-${index}`}>
+                <div className='div-checkbox-e-imagem'>
+                  <div className="content">
+                    <label className="checkBox">
+                      <input 
+                        id={`ch-${item.cartItemId}`} 
+                        type="checkbox" 
+                        checked={item.isSelected}
+                        onChange={() => toggleItem(item.cartItemId)}
+                      />
+                      <div className="transition" />
+                    </label>
+                  </div>
+                  
+                  <div className='div-imagem-produto-carrinho'>
+                    <div className='imagem-carrinho-logo'>
+                      <img className='imagem-mesmo-produtos-carrinho' src={item.image} alt={item.name} />
+                    </div>
+                  </div> 
+                  
+                  <div className='Descrição-estrela'> 
+                    <div className='descrição'>
+                      <div className="nome-avaliacao">
+                        <p className='nome-produto'>{item.name}</p>
+                      <div className='avaliacao-produto'>
+                        <StarRating rating={item.rating} />
+                        {/* <span className='quantidade-avaliacoes'>({item.reviewCount || 0})</span> */}
+                      </div>
+                      </div>
+                      
+                      {item.personalizacoes && Object.entries(item.personalizacoes).length > 0 && (
+                        <div className="personalizacoes-carrinho">
+                          {Object.entries(item.personalizacoes).map(([key, value]) => (
+                            <div key={key} className="personalizacao-item-carrinho">
+                              <span className="personalizacao-chave">{key.charAt(0).toUpperCase() + key.slice(1)}:</span>
+                              {key === 'cor' || key === 'corFundo' || key === 'corPrincipal' ? (
+                                <div 
+                                  className="cor-carrinho" 
+                                  style={{ backgroundColor: value.toLowerCase().replace(/á/g, 'a').replace(/é/g, 'e') }}
+                                  title={value}
+                                />
+                              ) : (
+                                <span className="personalizacao-valor">{value}</span>
+                              )}
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                    
+                    <div className='div-valor-brinquedo-carrinho'>
+                      <span className='cor-amarelo-preco-2'>R$:</span> {(item.price * (item.quantidade || 1)).toFixed(2)}
+                    </div>
+                  </div>
+                  
+                  <div className='div-quantidade-e-lixeira'>
+                    <div className="seletor-quantidade">
+                      {item.quantidade === 1 ? (
+                        <button onClick={() => removeFromCart(item.cartItemId)} className="trash-button">
+                          <img src={TrashIcon} alt="Remover" className="trash-icon" />
+                        </button>
+                      ) : (
+                        <button onClick={() => updateQuantity(item.cartItemId, Math.max(1, (item.quantidade || 1) - 1))}>-</button>
+                      )}
+                      <input 
+                          type="number" 
+                          value={item.quantidade || 1} 
+                          readOnly 
+                          min="1"
+                      />
+                      <button onClick={() => updateQuantity(item.cartItemId, (item.quantidade || 1) + 1)}>+</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          
+          <div className='div-button-carrinho'>
             <div className='div-total'>
-                Total: R${totalSelected.toFixed(2)}
+              Total: R${totalSelected.toFixed(2)}
             </div>
-         <div>
-            <Link to='/pagamento'><button>Comprar itens</button></Link>
-         </div>
-         </div>
-       </div>
-     </div>
-  </div>
+            <div>
+              <Link to='/pagamento'><button>Comprar itens</button></Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
